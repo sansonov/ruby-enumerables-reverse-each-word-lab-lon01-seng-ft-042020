caf3e7684277952592ece088ee76dade6c981137
Adao Natalino <sansonov@gmail.com>
2020-03-31 17:10:27 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def reverse_each_word(string)
+  array = string.split()
+  binding.pry
+  
+  array.each do |array|
+  return array.reverse!
+  end
+  
+  # array.collect do |k|
+  # k.reverse!
+  # end
+end

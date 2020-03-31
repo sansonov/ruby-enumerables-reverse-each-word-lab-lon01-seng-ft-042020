093e71a9e17ff60cd8173e200ba6b5ee1993bece
Adao Natalino <sansonov@gmail.com>
2020-03-31 17:20:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,13 +4,13 @@ def reverse_each_word(string)
   array = string.split()
   #binding.pry
   
-  # array.each do |k|
-  # k.reverse!
+  array.each do |k|
+  k.reverse
+  end.join(" ")
+  
+  
+  # array.collect do |k|
+  # k.reverse
   # end.join(" ")
-  
-  
-   array.collect do |k|
-   k.reverse
-   end.join(" ")
    
 end
